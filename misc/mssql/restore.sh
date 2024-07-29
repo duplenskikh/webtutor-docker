0839a999f39d61b8059914df9ffb990a5d03246a
Ivan Duplenskikh <693254+duplenskikh@users.noreply.github.com>
2024-07-29 18:11:44 +0200

    Change files after copying Fix sql check on database in container

--- misc/mssql/restore.sh
+++ misc/mssql/restore.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 timeout=3
+SQLCMD=/opt/mssql-tools/bin/sqlcmd
 
 while true;
 do
@@ -8,7 +9,7 @@ do
   sleep $timeout
 
   echo "Test connection to database"
-  /opt/mssql-tools/bin/sqlcmd -S host.docker.internal -U sa -P ${MSSQL_SA_PASSWORD} -Q "SELECT 1"
+  $SQLCMD -S host.docker.internal -U sa -P ${MSSQL_SA_PASSWORD} -Q "SELECT 1"
 
   if [ $? -eq 0 ]; then
     echo "Connection established"
@@ -18,17 +19,17 @@ done;
 
 if [ -e "/tmp/backup/db.bak" ]; then
   echo "Start database restoring"
-  /opt/mssql-tools/bin/sqlcmd -S host.docker.internal -U $MSSQL_USERNAME -P $MSSQL_SA_PASSWORD -Q 'RESTORE FILELISTONLY FROM DISK = "/var/opt/mssql/backup/db.bak"' | tr -s ' ' | cut -d ' ' -f 1-2 || exit 1
-  /opt/mssql-tools/bin/sqlcmd -S host.docker.internal -U $MSSQL_USERNAME -P $MSSQL_SA_PASSWORD -Q 'RESTORE DATABASE WTDB FROM DISK = "/var/opt/mssql/backup/db.bak" WITH MOVE "WTDB" TO "/var/opt/mssql/data/WTDB.mdf", MOVE "BLOBS" TO "/var/opt/mssql/data/WTDB_blobs.mdf", MOVE "FT_IDX" TO "/var/opt/mssql/data/WTDB_ft_idx.mdf", MOVE "IDX" TO "/var/opt/mssql/data/WTDB_idx.mdf", MOVE "LOG" TO "/var/opt/mssql/data/T1_WTDB.ldf"' || exit 1
+  $SQLCMD -S host.docker.internal -U $MSSQL_USERNAME -P $MSSQL_SA_PASSWORD -Q 'RESTORE FILELISTONLY FROM DISK = "/var/opt/mssql/backup/db.bak"' | tr -s ' ' | cut -d ' ' -f 1-2 || exit 1
+  $SQLCMD -S host.docker.internal -U $MSSQL_USERNAME -P $MSSQL_SA_PASSWORD -Q 'RESTORE DATABASE WTDB FROM DISK = "/var/opt/mssql/backup/db.bak" WITH MOVE "WTDB" TO "/var/opt/mssql/data/WTDB.mdf", MOVE "BLOBS" TO "/var/opt/mssql/data/WTDB_blobs.mdf", MOVE "FT_IDX" TO "/var/opt/mssql/data/WTDB_ft_idx.mdf", MOVE "IDX" TO "/var/opt/mssql/data/WTDB_idx.mdf", MOVE "LOG" TO "/var/opt/mssql/data/T1_WTDB.ldf"' || exit 1
 else
-  echo "Start database creating"
+  DATABASE_EXISTS=$($SQLCMD -S host.docker.internal -U sa -P ${MSSQL_SA_PASSWORD} -Q "IF EXISTS (SELECT name FROM sys.databases WHERE name = 'WTDB') PRINT 'EXISTS'")
 
-  /opt/mssql-tools/bin/sqlcmd -S host.docker.internal -U sa -P ${MSSQL_SA_PASSWORD} -Q "SELECT name FROM sys.database WHERE name = 'WTDB'"
-
-  if [ $? -eq 1 ]; then
-    /opt/mssql-tools/bin/sqlcmd -S host.docker.internal -U $MSSQL_USERNAME -P $MSSQL_SA_PASSWORD -i "/tmp/create.sql" || exit 1
+  if [[ $DATABASE_EXISTS != "EXISTS" ]]; then
+    echo "Database does not exist. Creating it..."
+    $SQLCMD -S host.docker.internal -U $MSSQL_USERNAME -P $MSSQL_SA_PASSWORD -i "/tmp/create.sql" || exit 1
+  else
+    echo "Database exist"
   fi
 fi
-
 
 exit 0
